d95a560376f2f25ad8aa2c9b2aac93f26f70b207
MateusADantas <mahawprogramacao@gmail.com>
2024-05-30 00:45:51 -0300

    project setup

---
+++ src/styles.ts
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from "styled-components";
+
+export const colors = {
+  darkerPurple: "#170D19",
+  darkerPurpleFooter: "#120B14",
+  darkPurple: "#35243D",
+  darkPurpleIcons: "#4B3456",
+  purpleTexts: "#644B72",
+  lightRed: "#FF6864",
+  lightPurple: "#826B8E",
+  white: "#FFFFFF",
+  lightGray: "#E6E3E8",
+  hotPink: "#E90083",
+  green: "#24D861",
+  red: "#DE3449",
+};
+
+export const breakpoints = {
+  desktop: "1024px",
+  tablet: "768px",
+};
+
+export const GlobalCss = createGlobalStyle`
+    
+    * {
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;  
+    list-style: none;
+    }
+`;
